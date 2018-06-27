fe91c2aba5641a355ce5c8b763097cb0ed356a8c
Duy Thanh Nguyen <ndthanhdev@outlook.com>
2018-06-27 10:07:52 +0700

    update consumer

--- src/services/consumer.js
+++ src/services/consumer.js
@@ -2,21 +2,22 @@
  * Project imports
  */
 const db = require('../db');
-const {createDebugLogger} = require('../utils');
+const { createDebugLogger } = require('../utils');
+const { pick } = require('ramda');
 
 // eslint-disable-next-line no-unused-vars
 const log = createDebugLogger('services:consumer');
 
-function create({address, userId}) {
-    return db.Consumer.create({address, userId});
+function create({ address, userId }) {
+    return db.Consumer.create({ address, userId });
 }
 
-function getConsumerByIdAndUserId({id, userId}) {
-    return db.Consumer.findOne({where: {id, userId}});
+function getConsumerByIdAndUserId({ id, userId }) {
+    return db.Consumer.findOne({ where: { id, userId } });
 }
 
 function getConsumerByUserId(userId) {
-    return db.Consumer.findAll({where: {userId}});
+    return db.Consumer.findAll({ where: { userId }, order: [['id', 'ASC']] });
 }
 
 function activateConsumer(consumerId) {
@@ -31,9 +32,24 @@ function activateConsumerHandler(...rest) {
     return activateConsumer(...rest);
 }
 
+function updateConsumerByIdAndUserId({ id, userId, newValue }) {
+    const updateAbleFields = ['address'];
+    const fields = Object.keys(pick(updateAbleFields, newValue));
+    return db.Consumer.update(newValue, {
+        // Return affected rows
+        returning: true,
+        where: {
+            id,
+            userId
+        },
+        fields
+    });
+}
+
 module.exports = {
     create,
     getConsumerByIdAndUserId,
     getConsumerByUserId,
-    activateConsumerHandler
+    activateConsumerHandler,
+    updateConsumerByIdAndUserId
 };
